4480e1dafec2234493ad42132ae378b0965b6d23
Badruzzaman <badru.cse@gmail.com>
2021-03-24 12:18:36 +0600

    BinaryTree

---
+++ BinaryTree.py
@@ -0,0 +1,10 @@
+
+class Node:
+    def __init__(self,data):
+        self.Left = None
+        self.Right = None
+        self.data = data
+    def printview(self):
+        print(self.data)
+Root = Node(10)
+Root.printview()
